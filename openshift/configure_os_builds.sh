a56372d22be9980c08640b9c1c1a5bce5d08c1ff
Michal Srb <michal@redhat.com>
2018-11-29 08:44:02 +0100

    Remove firehose-fetcher from deployment configs

--- openshift/configure_os_builds.sh
+++ openshift/configure_os_builds.sh
@@ -8,7 +8,7 @@ GITHUB_USERNAME="fabric8-analytics"
 
 templates="fabric8-analytics-jobs fabric8-analytics-server fabric8-analytics-data-model
 fabric8-analytics-worker fabric8-analytics-pgbouncer gremlin-docker
-fabric8-analytics-scaler fabric8-analytics-firehose-fetcher
+fabric8-analytics-scaler
 fabric8-analytics-license-analysis fabric8-analytics-stack-analysis
 f8a-server-backbone fabric8-analytics-stack-report-ui fabric8-analytics-api-gateway"
 
